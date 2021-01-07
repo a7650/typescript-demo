3a573b7bc97f41d91daa3f5ced5cd392dc537c67
ZZP <43945674+a7650@users.noreply.github.com>
2021-01-07 19:04:08 +0800

    update

--- 类型推断/demo.ts
+++ 类型推断/demo.ts
@@ -1,25 +1,44 @@
-(function () {
-    interface select {
-        a: string,
-        b?: number
+;(function () {
+  interface Ref<T> {
+    value: T
+    isRef: true
+  }
+  function ref<T>(value: T): Ref<T> {
+    return { value, isRef: true }
+  }
+
+  // function getVal<T extends any>(val: T): UnWrapRef<T> {
+  //   if (val.isRef) {
+  //     return getVal(val)
+  //   }
+  //   return val
+  // }
+
+  const val = 1
+
+  type val2 = UnWrapRef<typeof val>
+
+  type UnWrapRef<T> = T extends Ref<infer I> ? UnWrapRefSimple<I> : T
+  type UnWrapRefSimple<T> = T extends number ? T : UnWrapRefSimple<T>
+
+  interface select {
+    a: string
+    b?: number
+  }
+  let a: select = {
+    a: '1',
+    b: undefined
+  }
+
+  //字符串字面量类型
+  type Easing = 'in' | 'out' | 'in-out'
+  function animationUI(x: number, y: number, easing: Easing): void {
+    if (easing === 'in') {
+    } else if (easing === 'in-out') {
+    } else if (easing === 'out') {
+    } else {
+      //error
     }
-    let a: select = {
-        a: "1",
-        b: undefined
-    }
-
-    //字符串字面量类型
-    type Easing = "in" | "out" | "in-out"
-    function animationUI(x: number, y: number, easing: Easing): void {
-        if (easing === "in") {
-
-        } else if (easing === "in-out") {
-
-        } else if (easing === "out") {
-
-        } else {
-            //error
-        }
-    }
-    animationUI(10, 10, "in")
+  }
+  animationUI(10, 10, 'in')
 })()
